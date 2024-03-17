4cce05fea6d00941872bdd540812dfc5ae32c3e5
kxtzzl <123520267+romchhh@users.noreply.github.com>
2024-03-17 02:44:39 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = 'your_telegram_bot_token'
+GROUP_ID = 'your_telegram_group_id'
+ADMIN_IDS = ['admin_user_id1', 'admin_user_id2']
+SESSION_NAME = 'your_telegram_session_name'
+API_HASH = 'your_api_hash'
+API_ID = 'your_api_id'
+channels = ['channel_name1', 'channel_name2']
